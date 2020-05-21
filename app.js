847c9650ec17d2178a791b1032fbe5db1345e6b5
Robert Alestra <bobbyalestra135@gmail.com>
2020-05-20 22:48:56 -0400

    lesson9 adding event listeners

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const bookList = document.querySelector('#book-list');
+let btns = document.querySelectorAll('#book-list.delete');
 
-console.log('book-list next sibling is:', 
-bookList.nextSibling)
+Array.from(btns).forEach(function(btn){
+    btn.addEventListener('click', function(e){
 
-console.log('book-list next element sibling is:', 
-bookList.nextElementSibling)
+        const li = e.target.parentElement;
 
-console.log('book-list next sibling is:', 
-bookList.previousSibling)
+        li.parentNode.removeChild(li)
+    })
+})
 
-console.log('book-list next sibling is:', 
-bookList.previousElementSibling);
-
-
-bookList.previousElementSibling.querySelector('p.innerHTML += <br /> Too cool for everyone else')
+const link = document.querySelector('#page-banner a');
+link.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('navigation to ', e.target.textContent, 'was prevented')
+})
