58d4209f74df407df48328dda0a131d611bd173b
Robert Alestra <bobbyalestra135@gmail.com>
2020-05-20 23:07:46 -0400

    lesson11 working on event listener to the add book button

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const list = document.querySelector('#book-list ul');
 
-
+//delete button
 list.addEventListener('click', function(e){
 if(e.target.className == 'delete'){
     const li =e.target.parentElement;
     list.removeChild(li);
 }
+})
+
+// add button
+
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('Submit', function(e){
+    e.preventDefault();
+    const value = document.querySelector('input[type = "text"]').value
+    console.log(value)
 })
