61bf55303641af18d7f28b10ec108a3c1098bc5e
Robert Alestra <bobbyalestra135@gmail.com>
2020-05-25 15:43:48 -0400

    lesson17 adding the tabs and content

--- app.js
+++ app.js
@@ -45,23 +45,33 @@ addForm.addEventListener('submit',function(e){
 
 
    // how to make a searchbaR
-    const searchBar = document.forms['search-books'].querySelector('input');
-    
-    searchBar.addEventListener('keyup', function(e){
-        const term = e.target.value.toLowerCase();
+   const searchBar = forms['search-books'].querySelector('input');
+  searchBar.addEventListener('keyup', (e) => {
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach((book) => {
+      const title = book.firstElementChild.textContent;
+      if(title.toLowerCase().indexOf(e.target.value) != -1){
+        book.style.display = 'block';
+      } else {
+        book.style.display = 'none';
+      }
+    });
+  });
 
-        const books = list.getElementsByTagName('li');
-
-        Array.from(books).forEach(book);{
-            const title = book.firstElementChild.textContent;
-
-            if(title.toLowerCase().indexOf(term) != -1){
-                book.style.display = 'block';
-            }else{
-                book.style.display = 'none';
-            }
+  // tabbed content
+  const tabs = document.querySelector('.tabs');
+  const panels = document.querySelectorAll('.panel');
+  tabs.addEventListener('click', (e) => {
+    if(e.target.tagName == 'LI'){
+      const targetPanel = document.querySelector(e.target.dataset.target);
+      Array.from(panels).forEach((panel) => {
+        if(panel == targetPanel){
+          panel.classList.add('active');
+        }else{
+          panel.classList.remove('active');
         }
+      });
+    }
+  });
 
-    })
-
-  
